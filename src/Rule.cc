63a4b0dd93468a0b96a9fb21acb679fc9fbf1632
aryamaan-exe <aryamaan.27@gmail.com>
2026-06-04 16:13:26 -0500

    added rule validation

--- src/Rule.cc
+++ src/Rule.cc
@@ -1,5 +1,9 @@
 #include "Rule.hpp"
 
-Rule::Rule(std::string& name,
-           std::function<bool(std::vector<Phoneme*>&)>& condition):
+Rule::Rule(const std::string& name,
+           const std::function<bool(const std::vector<Phoneme*>&)>& condition):
     name_(name), condition_(condition) {}
+
+bool Rule::IsValid(const std::vector<Phoneme*>& syllable_part) {
+  return condition_(syllable_part);
+}
--- src/driver.cc
+++ src/driver.cc
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <vector>
+
 #include "Consonant.hpp"
+#include "Rule.hpp"
 #include "Syllable.hpp"
 #include "Vowel.hpp"
 
 int main() {
-  auto b = new Consonant("b", Place::Bilabial, Manner::Plosive, true);
-  auto a = new Vowel("a", Height::Low, Backness::Front, true, false);
-  auto r = new Consonant("ɹ", Place::Alveolar, Manner::Approximant, true);
+  auto* b = new Consonant("b", Place::Bilabial, Manner::Plosive, true);
+  auto* a = new Vowel("a", Height::Low, Backness::Front, true, false);
+  auto* r = new Consonant("ɹ", Place::Alveolar, Manner::Approximant, true);
   std::vector<Phoneme*> o{b};
   std::vector<Phoneme*> n{a};
   std::vector<Phoneme*> c{r};
+  std::vector<Phoneme*> e;
 
-  auto *bar = new Syllable(o, n, c);
+  auto* bar = new Syllable(o, n, c);
+  auto* rb = new Syllable(c, e, o);
   std::cout << bar->Symbols() << std::endl;
+
+  auto* rule = new Rule(
+      "Nucleus must exist",
+      [](const std::vector<Phoneme*>& nucleus) { return !nucleus.empty(); });
+
+  std::cout << std::boolalpha << rule->IsValid(bar->GetNucleus()) << std::endl;
+  std::cout << std::boolalpha << rule->IsValid(rb->GetNucleus()) << std::endl;
 }

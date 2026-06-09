af999381f77971959b44f2b11a2229fd2f58da8d
aryamaan-exe <aryamaan.27@gmail.com>
2026-06-09 17:11:46 -0500

    added single validation endpoint for all rules in language

--- includes/Rule.hpp
+++ includes/Rule.hpp
@@ -6,15 +6,23 @@
 
 #include "Phoneme.hpp"
 
+enum RuleType { Onset, Nucleus, Coda };
+
 class Rule {
 public:
   Rule(const std::string& name,
+       RuleType type,
        const std::function<bool(const std::vector<Phoneme*>&)>& condition);
 
   bool IsValid(const std::vector<Phoneme*>& syllable_part);
 
+  RuleType GetType();
+
+  std::string GetName();
+
 private:
   std::string name_;
+  RuleType type_;
   std::function<bool(const std::vector<Phoneme*>&)> condition_;
 };
 
--- src/Language.cc
+++ src/Language.cc
@@ -1,5 +1,8 @@
 #include "Language.hpp"
 
+#include <stdexcept>
+
+#include "Rule.hpp"
 #include "Syllable.hpp"
 
 void Language::AddPhoneme(Phoneme* phoneme) { inventory_.push_back(*phoneme); }
@@ -9,5 +12,25 @@ void Language::AddRule(const Rule* rule) { rules_.push_back(*rule); }
 bool Language::Validate(Word word) {
   std::vector<Syllable> syllables = word.GetSyllables();
 
-  // TODO: Check if rule type (onset rule, nucleus rule, coda rule) matches syllable part type
+  for (const Syllable& syllable : syllables) {
+    std::vector<Phoneme*> onset = syllable.GetOnset();
+    std::vector<Phoneme*> nucleus = syllable.GetNucleus();
+    std::vector<Phoneme*> coda = syllable.GetCoda();
+
+    for (Rule& rule : rules_) {
+      switch (rule.GetType()) {
+      case (RuleType::Onset):
+        if (!rule.IsValid(onset)) throw std::runtime_error(rule.GetName());
+        break;
+      case (RuleType::Nucleus):
+        if (!rule.IsValid(nucleus)) throw std::runtime_error(rule.GetName());
+        break;
+      case (RuleType::Coda):
+        if (!rule.IsValid(coda)) throw std::runtime_error(rule.GetName());
+        break;
+      }
+    }
+  }
+
+  return true;
 }

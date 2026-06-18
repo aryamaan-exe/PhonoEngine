ac2b43d2ed7c20ccedd981e8bba3cd9948716c9a
aryamaan-exe <aryamaan.27@gmail.com>
2026-06-18 18:25:48 -0500

    added word building from symbols

--- includes/Language.hpp
+++ includes/Language.hpp
@@ -1,3 +1,7 @@
+#ifndef LANGUAGE_HPP
+#define LANGUAGE_HPP
+
+#include <unordered_map>  
 #include "Phoneme.hpp"
 #include "Rule.hpp"
 #include "Word.hpp"
@@ -7,8 +11,13 @@ public:
   void AddPhoneme(Phoneme* phoneme);
   void AddRule(const Rule* rule);
   bool Validate(Word word);
+  Word BuildWordFromSymbols(std::vector<std::string>& word);
+  Phoneme GetPhonemeFromSymbol(std::string& symbol);
 
 private:
   std::vector<Phoneme> inventory_;
+  std::unordered_map<std::string, Phoneme> symbols_;
   std::vector<Rule> rules_;
 };
+
+#endif
--- src/Language.cc
+++ src/Language.cc
@@ -1,11 +1,16 @@
 #include "Language.hpp"
 
+#include <format>
+#include <memory>
 #include <stdexcept>
 
 #include "Rule.hpp"
 #include "Syllable.hpp"
 
-void Language::AddPhoneme(Phoneme* phoneme) { inventory_.push_back(*phoneme); }
+void Language::AddPhoneme(Phoneme* phoneme) {
+  inventory_.push_back(*phoneme);
+  symbols_.insert({phoneme->GetSymbol(), *phoneme});
+}
 
 void Language::AddRule(const Rule* rule) { rules_.push_back(*rule); }
 
@@ -18,19 +23,73 @@ bool Language::Validate(Word word) {
     std::vector<Phoneme*> coda = syllable.GetCoda();
 
     for (Rule& rule : rules_) {
-      switch (rule.GetType()) {
-      case (RuleType::Onset):
-        if (!rule.IsValid(onset)) throw std::runtime_error(rule.GetName());
-        break;
-      case (RuleType::Nucleus):
-        if (!rule.IsValid(nucleus)) throw std::runtime_error(rule.GetName());
-        break;
-      case (RuleType::Coda):
-        if (!rule.IsValid(coda)) throw std::runtime_error(rule.GetName());
-        break;
+      const auto& segment =
+          [&rule, &onset, &nucleus, &coda]() -> const std::vector<Phoneme*>& {
+        switch (rule.GetType()) {
+        case RuleType::Onset:
+          return onset;
+        case RuleType::Nucleus:
+          return nucleus;
+        case RuleType::Coda:
+          return coda;
+        }
+        throw std::logic_error("Invalid rule type");
+      }();
+
+      if (!rule.IsValid(segment)) {
+        throw std::runtime_error(
+            std::format("{} is invalid: {}", word.Symbols(), rule.GetName()));
       }
     }
   }
 
   return true;
+}
+
+Phoneme Language::GetPhonemeFromSymbol(std::string& symbol) {
+  if (!symbols_.contains(symbol)) {
+    throw std::invalid_argument(std::format(
+        "The symbol '{}' was not found in phonemic inventory.", symbol));
+  }
+
+  return symbols_.at(symbol);
+}
+
+Word Language::BuildWordFromSymbols(std::vector<std::string>& word) {
+  std::vector<Syllable> syllables;
+  std::vector<Phoneme*> onset;
+  std::vector<Phoneme*> nucleus;
+  std::vector<Phoneme*> coda;
+  std::vector<Phoneme*>* curr{&onset};
+  short it = -1;
+  for (const auto& c : word) {
+    if (c == ".") {
+      ++it;
+      it %= 3;
+      switch (it) {
+      case 0:
+        curr = &onset;
+        break;
+      case 1:
+        curr = &nucleus;
+        break;
+      case 2:
+        curr = &coda;
+        syllables.emplace_back(onset, nucleus, coda);
+        onset.clear();
+        nucleus.clear();
+        coda.clear();
+        break;
+      }
+    } else {
+      if (!symbols_.contains(c)) {
+        throw std::invalid_argument(std::format(
+            "The symbol '{}' was not found in phonemic inventory.", c));
+      }
+
+      curr->push_back(&symbols_.at(c));
+    }
+  }
+  Word result{syllables};
+  return result;
 }
